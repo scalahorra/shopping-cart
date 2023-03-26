2e34c8f3389d3a9e329da60d7ed389c0c8ed26d9
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-03-26 21:17:07 +0200

    reorder modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { IonicModule } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TabsComponent } from './components/tabs/tabs.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { ProfileModalComponent } from './components/profile-modal/profile-modal.component';
 import { FabButtonComponent } from './components/fab-button/fab-button.component';
 import { CreateListComponent } from './pages/create-list/create-list.component';
+
 
 @NgModule({
   declarations: [
