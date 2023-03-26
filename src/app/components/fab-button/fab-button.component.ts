2d448621e81625fa11719552b94e671444000d43
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-03-26 21:07:07 +0200

    add fab button

---
+++ src/app/components/fab-button/fab-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fab-button',
+  templateUrl: './fab-button.component.html',
+  styleUrls: ['./fab-button.component.scss']
+})
+export class FabButtonComponent {
+
+}
