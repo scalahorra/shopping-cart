6008882135ee8339147430d5ee0ee309b1fc0e50
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-04-02 23:55:41 +0200

    re do routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TabsComponent } from './components/tabs/tabs.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateListComponent } from './pages/create-list/create-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: TabsComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'create-list',
-        component: CreateListComponent
-      }
-    ]
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'create-list',
+    component: CreateListComponent
   }
 ];
 
